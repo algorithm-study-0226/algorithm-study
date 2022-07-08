4657d3a6a9bc81ccd0d5c725ec355d7c719daccd
Ju Yejin <linenive@naver.com>
2022-07-08 23:57:27 +0900

    [BOJ] Example / 브론즈5 / 1분
    https://www.acmicpc.net/problem/2557

---
+++ Yejin/example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <tuple>
+#define ll long long
+#define PI 3.14159265358979
+using namespace std;
+ll gcd(ll a, ll b) { return a % b ? gcd(b, a % b) : b; }
+ll lcm(ll a, ll b) { return (a * b) / gcd(a, b); }
+bool desc(int a, int b){ return a > b; }
+
+int main() {
+
+    return 0;
+}
