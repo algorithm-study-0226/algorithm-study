18bb604dd1b0f4faa6c5f0d6bc84c117992fcb16
woolee <lebind12@naver.com>
2022-07-14 10:22:40 +0900

    [BOJ] 수열의 합 / Silver 1 / 못풀었음

---
+++ Woolee/homework/1024.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void solve(int sum, int len) {
+
+}
+
+int main(void) {
+    int sum;
+    int len;
+
+    scanf("%d %d", &sum, &len);
+    solve(sum, len);
+
+    return (0);
+}
