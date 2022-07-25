e57672da5913ea4552ed04ccfccaeb8f5d197b38
woolee <lebind12@naver.com>
2022-07-26 07:57:54 +0900

    [BOJ] 암호 만들기 / Gold 5 / 2시간
    커밋 이름 잘못적어서 수정

--- Woolee/week2/homework/1759.c
+++ Woolee/week2/homework/1759.c
@@ -10,29 +10,80 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int get_str
+int picked_size;
 
-void sort(char *array, int size) {
+void sort_array(char *array, int size) {
     int i, j;
+    char tmp;
 
-    for (i = )
+    for (i = size - 1; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            if (array[j-1] > array[j]) {
+                tmp = array[j-1];
+                array[j-1] = array[j];
+                array[j] = tmp;
+            }   
+        }
+    }
 }
 
-void solve(char *array) {
+void print_array(char *array, int size) {
+    int i;
+    int count;
+    // 최소 한개의 모음과 두개의 자음이 있는지를 여기서 판단
+    // 모음의 갯수를 구해서 size에서 빼면 자음의 갯수.
+    count = 0;
+    for (i = 0; i < size; i++) {
+        if (array[i] == 'a' || array[i] == 'e' || array[i] == 'i' || array[i] == 'o' || array[i] == 'u')
+            count++;
+    }
+    if (count <= 0 || size - count <= 1)
+        return;
 
+    for (i = 0; i < size; i++) {
+        if (i == size - 1)
+            printf("%c\n", array[i]);
+        else
+            printf("%c", array[i]);
+    }
+}
+
+void pick(char *array, char *picked, int start, int topick, int size) {
+    if (topick == 0){
+        print_array(picked, picked_size);
+        return;
+    }
+
+    int i;
+
+    for (i = start; i < size; i++) {
+        picked[picked_size - topick] = array[i];
+        pick(array, picked, i + 1, topick - 1, size);
+    }
+}
+
+void solve(char *array, int L, int C) {
+    char picked[15];
+
+    pick(array, picked, 0, L, C);
 }
 
 int main(void) {
     int L;
     int C;
     int i;
-    char alpha[16];
+    char array[15];
 
     scanf("%d %d", &L, &C);
-    for (i = 0; i < C; i++)
-        scanf("%s", &alpha[i]);
-    solve(alpha);
+    for (i = 0; i < C; i++) {
+        scanf("%s", &array[i]);
+    }
+    picked_size = L;
+    sort_array(array, C);
+    // print_array(array, C);
+    solve(array, L, C);
 
     return (0);
 }
